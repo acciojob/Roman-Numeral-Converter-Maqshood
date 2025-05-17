309a02dcdaeb774175cd05dff954eb1a0b6caa4f
SK Maqshood <skmaqshood99@gmail.com>
2025-05-17 23:16:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,12 +9,18 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+let result = "";
+	for (let i = 0; i < obj.length; i++){
+		while (num>=obj[i][1]) {
+			result +=obj[i][0];
+			num-=obj[i][1];
+		}
+	}
+return result;
+}// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+ // console.log(convertToRoman(36));
 
-// console.log(convertToRoman(36));
 
 
 
